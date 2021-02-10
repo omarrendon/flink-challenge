076e53da6ef55b6324d823c40c8953a6abaa617e
omar rendon <omarcr.96@gmail.com>
2021-02-10 15:49:27 -0600

    Authentication and protected routes is done

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import reportWebVitals from "./reportWebVitals";
+
+import { Provider } from "react-redux";
+import { store } from "./redux/store/store";
+import Routes from "./routes/Routes";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <Routes />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
